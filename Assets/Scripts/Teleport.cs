f4c82d15d9f9aa1d210ca03a7461e818316952aa
Roisin-Memi-Grant-tus23 <k00293226@student.tus.ie>
2026-08-17 19:26:03 +0100

    Teleport

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Teleport : MonoBehaviour
+{
+    public Transform player, destination;
+    public GameObject playerg;
+   
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerg.SetActive(false);
+            player.position = destination.position;
+            playerg.SetActive(true);
+        }
+    }
+}
